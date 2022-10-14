ff2dca97943c15fc7972c78c19f0ba326cbdef84
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2022-10-14 10:56:52 -0500

    Creation and Set Up of Sprint2
    Creation of Sprint2 Product and Test packages with prepared but empty class and test files

---
+++ Sprint2/src/Product/Main.java
@@ -0,0 +1,9 @@
+package Product;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ Sprint2/src/Test/SOSBoardTest.java
@@ -0,0 +1,10 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SOSBoardTest {
+
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {
+  }
+}
