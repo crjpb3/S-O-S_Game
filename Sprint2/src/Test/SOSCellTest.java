80866dcebe422c9d6912b687fdcece31158c9430
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2022-10-20 16:06:47 -0500

    Wrote test cases for SOSCell class
    Wrote 2 test cases for SOSCell class

--- Sprint2/src/Test/SOSCellTest.java
+++ Sprint2/src/Test/SOSCellTest.java
@@ -1,7 +1,28 @@
 package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import Product.SOSCell;
 
 class SOSCellTest {
+  @Test
+  void testIsEmpty(){
+    SOSCell Cell1 = new SOSCell();
+    assertEquals(true, Cell1.isEmpty());
 
+    Cell1.setContent(CellContent.S_CHAR);
+    assertEquals(false, Cell1.isEmpty());
+  }
+
+  @Test
+  void testGetContent(){
+    SOSCell Cell1 = new SOSCell();
+    
+    Cell1.setContent(CellContent.X_CHAR);
+    assertEquals(CellContent.X_CHAR, Cell1.getContent());
+
+    Cell1.setContent(CellContent.O_CHAR);
+    assertEquals(CellContent.O_CHAR, Cell1.getContent());
+
+  }
 }
