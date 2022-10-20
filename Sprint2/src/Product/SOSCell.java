bb54ef11ef61854f8ae2ebaa9c4563dc21093e73
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2022-10-20 16:47:46 -0500

    Completed writing SOSCell to pass testing

--- Sprint2/src/Product/SOSCell.java
+++ Sprint2/src/Product/SOSCell.java
@@ -1,7 +1,21 @@
 package Product;
 
 public class SOSCell{
+  public enum CellContent {NULL, S_CHAR, O_CHAR}
+  CellContent currentContent;
   public SOSCell(){
+    this.setContent(CellContent.NULL);
+  }
 
+  public boolean isEmpty(){
+    return currentContent == CellContent.NULL;
+  }
+
+  public void setContent(CellContent content){
+    currentContent = content;
+  }
+  
+  public CellContent getContent(){
+    return currentContent;
   }
 }
