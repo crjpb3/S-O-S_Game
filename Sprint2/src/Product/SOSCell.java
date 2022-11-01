0e8fc44233e9257ea4a7557cbb7cab9b6aae6ece
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2022-11-01 18:18:44 -0500

    Adjusted SOSCell class
    Adapted SOSCell class to accommodate functions the GUI can use to determine where to draw SOS lines

--- Sprint2/src/Product/SOSCell.java
+++ Sprint2/src/Product/SOSCell.java
@@ -1,7 +1,11 @@
 package Product;
 
 public class SOSCell{
-  String currentContent;
+  private String currentContent;
+  private boolean isSOS = false;
+  private int cellOwnerID = -1;
+  private int beginIndexOfSOS = -1;
+  private int endIndexOfSOS = -1;
   public SOSCell(){
     this.setContent("");
   }
@@ -16,5 +20,42 @@ public class SOSCell{
   
   public String getContent(){
     return currentContent;
+  }
+
+  public void setIsSOS(){
+    if(isSOS){
+      isSOS = false;
+    }
+    else{
+      isSOS = true;
+    }
+  }
+
+  public boolean isSOS(){
+    return isSOS;
+  }
+
+  public void setCellOwner(int playerID){
+    cellOwnerID = playerID;
+  }
+
+  public int getCellOwner(){
+    return cellOwnerID;
+  }
+
+  public void setBeginIndexOfSOS(int beginIndex){
+    beginIndexOfSOS = beginIndex;
+  }
+
+  public void setEndIndexOfSOS(int endIndex){
+    endIndexOfSOS = endIndex;
+  }
+
+  public int getBeginIndexOfSOS(){
+    return beginIndexOfSOS;
+  }
+
+  public int getEndIndexOfSOS(){
+    return endIndexOfSOS;
   }
 }
