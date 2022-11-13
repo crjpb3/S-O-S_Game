535997732e7b1b3b0767b397755e6d8eea0b126f
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2022-11-13 16:18:50 -0600

    Adjusted and added tests for AC 8 and 9
    Adjusted SOSGame() constructor to accommodate computer players and corrected calls in the test file. Added tests for AC 8 and 9 with computer players

--- Sprint2/src/Test/SOSGameTest.java
+++ Sprint2/src/Test/SOSGameTest.java
@@ -11,40 +11,40 @@ class SOSGameTest {
   void testBoardSize(){
 
     //AC 1.1
-    SOSGame Game1 = new SOSGame(5,0);
+    SOSGame Game1 = new SOSGame(5,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(5, Game1.getBoardSize());
 
     //AC 1.2
-    SOSGame Game2 = new SOSGame(-5,0);
+    SOSGame Game2 = new SOSGame(-5,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(3, Game2.getBoardSize());
 
     //AC 1.3
-    SOSGame Game3 = new SOSGame(5.5,0);
+    SOSGame Game3 = new SOSGame(5.5,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(5, Game3.getBoardSize());
 
     //AC 1.4
-    SOSGame Game4 = new SOSGame(50,0);
+    SOSGame Game4 = new SOSGame(50,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(10, Game4.getBoardSize());
 
     //AC 1.5
-    SOSGame Game5 = new SOSGame('z',0);
+    SOSGame Game5 = new SOSGame('z',0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(3, Game5.getBoardSize());
   }
 
   @Test
   void testGameModeSelection(){
     //AC 2.1
-    SOSGame Game1 = new SOSGame(3,0);
+    SOSGame Game1 = new SOSGame(3,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals("Simple", Game1.getGameMode());
 
     //AC 2.2
-    SOSGame Game2 = new SOSGame(3,1);
+    SOSGame Game2 = new SOSGame(3,1, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals("General", Game2.getGameMode());
   }
 
   @Test
   void testGameStart(){
-    SOSGame Game = new SOSGame(3,0);
+    SOSGame Game = new SOSGame(3,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
 
     //AC 3.0
     assertEquals(3, Game.getBoardSize());
@@ -54,7 +54,7 @@ class SOSGameTest {
 
   @Test
   void testPlayerMove(){
-    SOSGame Game = new SOSGame(0,3);
+    SOSGame Game = new SOSGame(0,3, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
 
     //AC 4 & 6 Incomplete implementation testing
     //AC 4.1 & 6.1
@@ -83,7 +83,7 @@ class SOSGameTest {
 
   @Test
   void testGetGeneralGameScore(){
-    SOSGame Game = new SOSGame(3,1);
+    SOSGame Game = new SOSGame(3,1, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
 
     //AC 6.3
     //Player 1 scoring
@@ -97,7 +97,7 @@ class SOSGameTest {
 
     //AC 6.4
     //Player 2 scoring
-    Game.resetGame(3,1);
+    Game.resetGame(3,1, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(0,Game.getGeneralGameScore(SOSGame.Turn.PL2));//Show Player 2 score equals 0 at start
     Game.makeMove(1,0, "S");//Player 1 move
     Game.makeMove(0,0, "S");//Player 2 move
@@ -111,7 +111,7 @@ class SOSGameTest {
   @Test
   void testPlayer1SimpleWin(){
     //AC 5.1
-    SOSGame Game = new SOSGame(3,0);
+    SOSGame Game = new SOSGame(3,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(Status.PLAYING, Game.getGameStatus());//Show no win status
 
     Game.makeMove(0,0,"S");//Player 1 move
@@ -126,7 +126,7 @@ class SOSGameTest {
   @Test
   void testPlayer2SimpleWin(){
     //AC 5.2
-    SOSGame Game = new SOSGame(3,0);
+    SOSGame Game = new SOSGame(3,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(Status.PLAYING, Game.getGameStatus());//Show no win status
 
     Game.makeMove(1,0,"S");//Player 1 move
@@ -143,7 +143,7 @@ class SOSGameTest {
   @Test
   void testSimpleDraw(){
     //AC 5.3
-    SOSGame Game = new SOSGame(3,0);
+    SOSGame Game = new SOSGame(3,0, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(SOSGame.Status.PLAYING, Game.getGameStatus());
     //Fill all cells without creating an SOS
     for(int i = 0; i < Game.getBoardSize(); i++){
@@ -157,7 +157,7 @@ class SOSGameTest {
   @Test
   void testPlayer1GeneralWin(){
     //AC 7.1
-    SOSGame Game = new SOSGame(3,1);
+    SOSGame Game = new SOSGame(3,1, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(SOSGame.Status.PLAYING,Game.getGameStatus());//Show game has not ended
     assertEquals(0, Game.getGeneralGameScore(SOSGame.Turn.PL1));//Show player 1 score at 0
     assertEquals(0, Game.getGeneralGameScore(SOSGame.Turn.PL2));//Show player 2 score at 0
@@ -181,7 +181,7 @@ class SOSGameTest {
   @Test
   void testPlayer2GeneralWin(){
     //AC 7.2
-    SOSGame Game = new SOSGame(3,1);
+    SOSGame Game = new SOSGame(3,1, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(SOSGame.Status.PLAYING,Game.getGameStatus());//Show game has not ended
     assertEquals(0, Game.getGeneralGameScore(SOSGame.Turn.PL1));//Show player 1 score at 0
     assertEquals(0, Game.getGeneralGameScore(SOSGame.Turn.PL2));//Show player 2 score at 0
@@ -204,7 +204,7 @@ class SOSGameTest {
   @Test
   void testGeneralDraw(){
     //AC 7.3
-    SOSGame Game = new SOSGame(3,1);
+    SOSGame Game = new SOSGame(3,1, SOSGame.PlayerType.HUMAN, SOSGame.PlayerType.HUMAN);
     assertEquals(SOSGame.Status.PLAYING, Game.getGameStatus());
     //Fill all cells without creating an SOS
     for(int i = 0; i < Game.getBoardSize(); i++){
@@ -213,5 +213,55 @@ class SOSGameTest {
       }
     }
     assertEquals(SOSGame.Status.DRAW, Game.getGameStatus());
+  }
+
+  @Test
+  void testPlayerTypeSelection(){
+    SOSGame Game = new SOSGame(3,0,SOSGame.PlayerType.HUMAN,SOSGame.PlayerType.HUMAN);
+    assertEquals(SOSGame.PlayerType.HUMAN, Game.getPlayerType(SOSGame.Turn.PL1));
+    assertEquals(SOSGame.PlayerType.HUMAN, Game.getPlayerType(SOSGame.Turn.PL2));
+
+    Game.resetGame(3,0,SOSGame.PlayerType.COMPUTER,SOSGame.PlayerType.HUMAN);
+    assertEquals(SOSGame.PlayerType.COMPUTER, Game.getPlayerType(SOSGame.Turn.PL1));
+    assertEquals(SOSGame.PlayerType.HUMAN, Game.getPlayerType(SOSGame.Turn.PL2));
+
+    Game.resetGame(3,0,SOSGame.PlayerType.HUMAN,SOSGame.PlayerType.COMPUTER);
+    assertEquals(SOSGame.PlayerType.HUMAN, Game.getPlayerType(SOSGame.Turn.PL1));
+    assertEquals(SOSGame.PlayerType.COMPUTER, Game.getPlayerType(SOSGame.Turn.PL2));
+
+    Game.resetGame(3,0,SOSGame.PlayerType.COMPUTER,SOSGame.PlayerType.COMPUTER);
+    assertEquals(SOSGame.PlayerType.COMPUTER, Game.getPlayerType(SOSGame.Turn.PL1));
+    assertEquals(SOSGame.PlayerType.COMPUTER, Game.getPlayerType(SOSGame.Turn.PL2));
+  }
+
+  @Test
+  void testComputerMove(){
+    SOSGame Game = new SOSGame(3,0,SOSGame.PlayerType.HUMAN,SOSGame.PlayerType.HUMAN);
+    int x = Game.computerChooseX();
+    int y = Game.computerChooseY();
+    String token = Game.computerChooseToken();
+
+    Game.makeMove(x,y,token);
+    assertFalse(Game.isCellEmpty(x,y));
+    assertEquals(token, Game.getCellContent(x,y));
+  }
+  @Test
+  void test1ComputerPlayerSimple(){
+    SOSGame Game = new SOSGame(3,0,SOSGame.PlayerType.HUMAN,SOSGame.PlayerType.COMPUTER);
+  }
+
+  @Test
+  void test1ComputerPlayerGeneral(){
+    SOSGame Game = new SOSGame(3,1,SOSGame.PlayerType.HUMAN,SOSGame.PlayerType.COMPUTER);
+  }
+
+  @Test
+  void test2ComputerPlayersSimple(){
+    SOSGame Game = new SOSGame(3,0,SOSGame.PlayerType.COMPUTER,SOSGame.PlayerType.COMPUTER);
+  }
+
+  @Test
+  void test2ComputerPlayersGeneral(){
+    SOSGame Game = new SOSGame(3,1,SOSGame.PlayerType.COMPUTER,SOSGame.PlayerType.COMPUTER);
   }
 }
