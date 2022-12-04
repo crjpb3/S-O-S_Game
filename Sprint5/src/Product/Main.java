7cd3d5da2274d10b69f06e857b1a9b33a8aa1f0c
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2022-12-04 11:07:00 -0600

    Created Sprint 5 Directory and Files
    Created Sprint 5 directory using files from sprint2 to continue building on top of

---
+++ Sprint5/src/Product/Main.java
@@ -0,0 +1,8 @@
+package Product;
+
+public class Main {
+
+  public static void main(String[] args) {
+    SOSGui GUI = new SOSGui();
+  }
+}
---
+++ Sprint5/src/Product/SOSCell.java
@@ -0,0 +1,63 @@
+package Product;
+
+import java.util.Objects;
+
+public class SOSCell {
+
+  private String currentContent;
+  private int cellOwnerID = -1;
+  private int beginRowIndex = -1;
+  private int endRowIndex = -1;
+  private int beginColIndex = -1;
+  private int endColIndex = -1;
+
+  public SOSCell() {
+    this.setContent("");
+  }
+
+  public boolean isEmpty() {
+    return Objects.equals(this.currentContent, "");
+  }
+
+  public void setContent(String content) {
+    this.currentContent = content;
+  }
+
+  public String getContent() {
+    return this.currentContent;
+  }
+
+  public void setCellOwner(int playerID) {
+    this.cellOwnerID = playerID;
+  }
+
+  public int getCellOwner() {
+    return this.cellOwnerID;
+  }
+
+  public void setBeginIndexOfSOS(int row, int col) {
+    this.beginRowIndex = row;
+    this.beginColIndex = col;
+  }
+
+  public void setEndIndexOfSOS(int row, int col) {
+    this.endRowIndex = row;
+    this.endColIndex = col;
+  }
+
+  public int getBeginRowIndex() {
+    return this.beginRowIndex;
+  }
+
+  public int getEndRowIndex() {
+    return this.endRowIndex;
+  }
+
+  public int getBeginColIndex() {
+    return this.beginColIndex;
+  }
+
+  public int getEndColIndex() {
+    return this.endColIndex;
+  }
+}
